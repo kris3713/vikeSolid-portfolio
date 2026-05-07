2c0799731cb555d88946d7ffd8ffceb75a97f42c
kris3713 <07e5297d5b@c0x0.com>
2026-05-07 00:19:01 -0600

    Updates
    Signed-off-by: kris3713 <07e5297d5b@c0x0.com>

--- pages/+config.ts
+++ pages/+config.ts
@@ -1,11 +1,11 @@
 import vikeSolid from 'vike-solid/config';
-import type { Config } from 'vike/types';
+import { defineConfig, type Config } from 'vike/types';
 
 // Default config (can be overridden by pages)
 // https://vike.dev/config
-export default <Config>{
+export default defineConfig<Config>({
   // https://vike.dev/head-tags
   title: `Kris Schneider's portfolio`,
   description: `A show case of my work and myself.`,
   extends: [vikeSolid]
-};
+});
