91f57f59a5009dee67e8b9378ba8128ef8010073
kris3713 <07e5297d5b@c0x0.com>
2026-05-13 15:59:04 -0600

    Added helper lib
    Signed-off-by: kris3713 <07e5297d5b@c0x0.com>

---
+++ lib/tags.ts
@@ -0,0 +1,26 @@
+// https://github.com/digitalservicebund/ris-ui/blob/613c87e39f88f76e322463f339892be094a9f204/src/lib/tags.ts
+const tag = (strings: TemplateStringsArray, ...values: unknown[]) =>
+  String.raw({ raw: strings }, ...values);
+
+/**
+ * Tagged template string for HTML. The tag itself doesn't do anything, but
+ * using it will allow your editor to provide HTML-features for template
+ * strings in scripts, e.g. stories:
+ *
+ * ```ts
+ * const template = html`<div>Hello world</div>`
+ * ```
+ */
+export const html = tag;
+
+/**
+ * Tagged template string for Tailwind classes. The tag itself doesn't do
+ * anything, but using it will allow your editor to provide Tailwind
+ * Intellisense for template strings in scripts, as well as automatic class
+ * sorting via Prettier.
+ *
+ * ```ts
+ * const classes = tw`px-16 bg-blue-200`
+ * ```
+ */
+export const tw = tag;
