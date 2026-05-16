eb73343a1a0a5f35d650e920aa24ac4ca6287534
kris3713 <07e5297d5b@c0x0.com>
2026-05-16 02:07:48 -0600

    Updated the projects page and made it more accessible
    Signed-off-by: kris3713 <07e5297d5b@c0x0.com>

--- pages/projects/+Page.tsx
+++ pages/projects/+Page.tsx
@@ -9,42 +9,70 @@ import projects from './data';
 export default function Page() {
   return (
     <div
+      id='projects-page'
       class={cx(
         'flex flex-col relative gap-14',
         'justify-between w-full',
         'min-h-(--min-height) p-(--padding)'
       )}
+      aria-label='Projects Page'
     >
       <h1 id='projects'>Projects</h1>
       <div class='flex flex-col flex-1 gap-6 min-w-0 w-full'>
         <div class='grid grid-cols-[repeat(2,1fr)] gap-5'>
           <For each={projects}>
-            {({ id, title, desc, link, git_url }) => (
+            {({ id, title, desc, tech_stacks, link, git_url }) => (
               <div class='project-card' id={id}>
                 <h2 id='project-title'>{title}</h2>
-                <p
-                  id='project-desc'
+                <p id='project-desc'>{desc}</p>
+
+                <ul
+                  id='project-tech-stacks'
                   class={cx(
-                    'text-(size:--desc-size) text-active',
-                    'transition-colors leading-relaxed',
-                    'text-wrap text-clip overflow-hidden'
+                    'flex flex-wrap gap-4.5',
+                    'wrap-normal self-start',
+                    'mt-auto'
                   )}
+                  aria-label='Project Tech Stacks'
                 >
-                  {desc}
-                </p>
+                  <For each={tech_stacks}>
+                    {tech_stack => (
+                      <li
+                        id={tech_stack}
+                        class={cx(
+                          'text-primary bg-primary/11',
+                          'rounded-md px-2 py-1'
+                        )}
+                        aria-label='Project Tech Stack'
+                      >
+                        {tech_stack}
+                      </li>
+                    )}
+                  </For>
+                </ul>
+
                 <div
                   id='links'
                   class={cx(
                     'flex items-center gap-4.5',
                     'self-end mt-auto'
                   )}
+                  aria-label='Links'
                 >
                   {link && (
-                    <a href={link} target='_blank'>
-                      <ExternalLink width={24} height={24} viewBox='0 0 24 24' />
+                    <a
+                      href={link}
+                      target='_blank'
+                      aria-label='External Link'
+                    >
+                      <ExternalLink size={24} viewBox='0 0 24 24' />
                     </a>
                   )}
-                  <a href={git_url} target='_blank'>
+                  <a
+                    href={git_url}
+                    target='_blank'
+                    aria-label='Git Repository'
+                  >
                     <img width={24} height={24} src={GitHub} />
                   </a>
                 </div>
