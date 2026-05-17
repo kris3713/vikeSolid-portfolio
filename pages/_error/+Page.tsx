9be58c8485ed930490b33b4b652e1470f693fe6f
kris3713 <07e5297d5b@c0x0.com>
2026-05-17 14:10:44 -0600

    Improved error page
    Signed-off-by: kris3713 <07e5297d5b@c0x0.com>

--- pages/_error/+Page.tsx
+++ pages/_error/+Page.tsx
@@ -1,21 +1,32 @@
+import './styles.css'
 import { Show } from 'solid-js';
+import { cx } from 'tailwind-variants/lite';
 import { usePageContext } from 'vike-solid/usePageContext';
 
 export default function Page() {
-  const { is404 } = usePageContext();
+  const pageContext = usePageContext();
+  const is404 = () => pageContext.is404;
+
   return (
-    <div id='error-page'>
+    <div
+      id='error-page'
+      class={cx(
+        'flex flex-col relative gap-14',
+        'w-full min-h-(--min-height) p-(--padding)'
+      )}
+      aria-label='Error Page'
+    >
       <Show
         when={is404}
-        // fallback={
-        //   <>
-        //     <h1>Internal Error</h1>
-        //     <p>Something went wrong.</p>
-        //   </>
-        // }
+        fallback={
+          <>
+            <h1 id='error'>Internal Error</h1>
+            <p id='error-message'>Something went wrong.</p>
+          </>
+        }
       >
-        <h1>Page Not Found</h1>
-        <p>This page could not be found.</p>
+        <h1 id='error'>Page Not Found</h1>
+        <p id='error-message'>This page could not be found.</p>
       </Show>
     </div>
   );
