823b63f90abce15a63123fff4aae6bda6637258d
kris3713 <07e5297d5b@c0x0.com>
2026-05-17 02:34:56 -0600

    Simplifed projects page
    Signed-off-by: kris3713 <07e5297d5b@c0x0.com>

--- pages/projects/+Page.tsx
+++ pages/projects/+Page.tsx
@@ -6,106 +6,104 @@ import { cx } from 'tailwind-variants/lite';
 import GitHub from '../../assets/github.svg';
 import projects from './data';
 
-export default function Page() {
-  return (
-    <div
-      id='projects-page'
-      class={cx(
-        'flex flex-col relative gap-14',
-        'justify-between w-full',
-        'min-h-(--min-height) p-(--padding)'
-      )}
-      aria-label='Projects Page'
-    >
-      <h1 id='projects'>Projects</h1>
-      <div class='flex flex-row md:flex-col flex-1 gap-6 w-full'>
-        <div
-          class={cx(
-            'grid gap-5 grid-rows-[repeat(2,1fr)]',
-            'md:grid-cols-[repeat(2,1fr)]'
-          )}
-        >
-          <For each={projects}>
-            {({ id, title, desc, tech_stacks, link, git_url }) => (
-              <div class='project-card' id={id}>
-                <h2 id='project-title'>{title}</h2>
-                <p id='project-desc'>{desc}</p>
+const Page = () => (
+  <div
+    id='projects-page'
+    class={cx(
+      'flex flex-col relative gap-14',
+      'justify-between w-full',
+      'min-h-(--min-height) p-(--padding)'
+    )}
+    aria-label='Projects Page'
+  >
+    <h1 id='projects'>Projects</h1>
+    <div class='flex flex-row md:flex-col flex-1 gap-6 w-full'>
+      <div
+        class={cx(
+          'grid gap-5 grid-rows-[repeat(2,1fr)]',
+          'md:grid-cols-[repeat(2,1fr)]'
+        )}
+      >
+        <For each={projects}>
+          {({ id, title, desc, tech_stacks, link, git_url }) => (
+            <div class='project-card' id={id}>
+              <h2 id='project-title'>{title}</h2>
+              <p id='project-desc'>{desc}</p>
 
-                <ul
-                  id='project-tech-stacks'
-                  class={cx(
-                    'flex flex-wrap gap-4.5',
-                    'wrap-normal self-start',
-                    'mt-auto'
-                  )}
-                  aria-label='Project Tech Stacks'
-                >
-                  <For each={tech_stacks}>
-                    {tech_stack => (
-                      <li
-                        id={tech_stack
-                          .replace(' ', '_')
-                          .toLowerCase()}
-                        class={cx(
-                          'text-primary bg-primary/11',
-                          'rounded-md px-2 py-1'
-                        )}
-                        aria-label={tech_stack}
-                      >
-                        {tech_stack}
-                      </li>
-                    )}
-                  </For>
-                </ul>
-
-                <div
-                  id='links'
-                  class={cx(
-                    'flex items-center gap-4.5',
-                    'self-end mt-auto'
-                  )}
-                  aria-label='Links'
-                >
-                  {link && (
-                    <a
-                      href={link}
-                      target='_blank'
-                      aria-label='External Link'
+              <ul
+                id='project-tech-stacks'
+                class={cx(
+                  'flex flex-wrap gap-4.5',
+                  'wrap-normal self-start',
+                  'mt-auto'
+                )}
+                aria-label='Project Tech Stacks'
+              >
+                <For each={tech_stacks}>
+                  {tech_stack => (
+                    <li
+                      id={tech_stack.replace(' ', '_').toLowerCase()}
+                      class={cx(
+                        'text-primary bg-primary/11',
+                        'rounded-md px-2 py-1'
+                      )}
+                      aria-label={tech_stack}
                     >
-                      <ExternalLink size={24} viewBox='0 0 24 24' />
-                    </a>
+                      {tech_stack}
+                    </li>
                   )}
+                </For>
+              </ul>
+
+              <div
+                id='links'
+                class={cx(
+                  'flex items-center gap-4.5',
+                  'self-end mt-auto'
+                )}
+                aria-label='Links'
+              >
+                {link && (
                   <a
-                    href={git_url}
+                    href={link}
                     target='_blank'
-                    aria-label='Git Repository'
+                    aria-label='External Link'
                   >
-                    <img width={24} height={24} src={GitHub} />
+                    <ExternalLink size={24} viewBox='0 0 24 24' />
                   </a>
-                </div>
+                )}
+                <a
+                  href={git_url}
+                  target='_blank'
+                  aria-label='Git Repository'
+                >
+                  <img width={24} height={24} src={GitHub} />
+                </a>
               </div>
-            )}
-          </For>
-        </div>
+            </div>
+          )}
+        </For>
       </div>
-      {/* <div */}
-      {/*   class={cx( */}
-      {/*     'flex items-center relative overflow-hidden', */}
-      {/*     'min-h-(--min-height) p-(--padding)' */}
-      {/*   )} */}
-      {/* > */}
-      {/*   <div */}
-      {/*     class={cx( */}
-      {/*       'flex flex-row items-start', */}
-      {/*       'justify-between w-full gap-14' */}
-      {/*     )} */}
-      {/*   > */}
-      {/*     <div class='flex-1 flex flex-col gap-8 min-w-0'> */}
-      {/*       <div class='flex flex-col gap-3'> */}
-      {/*       </div> */}
-      {/*     </div> */}
-      {/*   </div> */}
-      {/* </div> */}
     </div>
-  );
-}
+    {/* <div */}
+    {/*   class={cx( */}
+    {/*     'flex items-center relative overflow-hidden', */}
+    {/*     'min-h-(--min-height) p-(--padding)' */}
+    {/*   )} */}
+    {/* > */}
+    {/*   <div */}
+    {/*     class={cx( */}
+    {/*       'flex flex-row items-start', */}
+    {/*       'justify-between w-full gap-14' */}
+    {/*     )} */}
+    {/*   > */}
+    {/*     <div class='flex-1 flex flex-col gap-8 min-w-0'> */}
+    {/*       <div class='flex flex-col gap-3'> */}
+    {/*       </div> */}
+    {/*     </div> */}
+    {/*   </div> */}
+    {/* </div> */}
+  </div>
+);
+
+export default Page;
