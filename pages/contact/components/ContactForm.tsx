5bdf339038e7ea41b28be46960ac11d7e2fe08b7
kris3713 <07e5297d5b@c0x0.com>
2026-05-18 16:06:31 -0600

    Completed contact form component
    Signed-off-by: kris3713 <07e5297d5b@c0x0.com>

--- pages/contact/components/ContactForm.tsx
+++ pages/contact/components/ContactForm.tsx
@@ -1,4 +1,4 @@
-import { createSignal } from 'solid-js';
+import { createSignal, For } from 'solid-js';
 import { cx } from 'tailwind-variants/lite';
 
 import GenContainer from './GenContainer';
@@ -6,63 +6,98 @@ import GenContainer from './GenContainer';
 type FormData = {
   name: string;
   email: string;
-  meessage: string;
+  message: string;
 };
 
-type FormEvent = SubmitEvent & {
-  currentTarget: HTMLFormElement;
-  target: Element;
-};
-
-type ChangeEvent = Event & {
-  currentTarget: HTMLInputElement;
-  target: HTMLInputElement;
+type ChangeEvent = InputEvent & {
+  /** There is a chance that `target` is possibly null. */
+  target: HTMLInputElement | null;
 };
 
 export default function ContactForm() {
-  const [formData, setFormData] = createSignal<FormData>({
+  const defaults = {
     name: '',
     email: '',
-    meessage: ''
-  });
+    message: ''
+  };
 
-  function handleSubmit(event: FormEvent) {
+  // const capFirstLetter = (str: string) =>
+  //   str.charAt(0).toUpperCase() + str.slice(1);
+
+  const [formData, setFormData] = createSignal<FormData>(defaults);
+
+  function handleSubmit(event: SubmitEvent) {
     event.preventDefault();
     console.log('Form submitted:', formData());
-    setFormData({
-      name: '',
-      email: '',
-      meessage: ''
-    });
+    setFormData(defaults);
   }
 
-  function handleChange(event: ChangeEvent) {
-    console.log('Form changed:', formData());
-    setFormData({
-      ...formData(),
-      [event.target.name]: event.target.value
-    });
+  // TODO: Implement a send to email
+  // TODO: functionality after form submission.
+
+  function handleInput(event: ChangeEvent) {
+    const target = event.target;
+    if (target) {
+      const { name, value } = target;
+      console.log('Input changed:', { [name]: value });
+      setFormData({
+        ...formData(),
+        [name]: value
+      });
+    }
   }
 
   return (
     <GenContainer id='contact-form' aria-label='Contact Form'>
       <form onSubmit={handleSubmit}>
-        <label for='name' class='block mb-2 text-active'>
-          Name
-        </label>
-        <input
-          type='text'
-          id='name'
-          name='text'
-          value={formData().name}
-          onChange={handleChange}
-          required={true}
-          class={cx(
-            'w-full bg-input border border-border rounded-lg',
-            'px-4 py-3 focus:outline-none',
-            'focus:ring-2 focus:ring-primary'
+        <For each={['name', 'email']}>
+          {str => (
+            <div id={`${str}-input`} class='flex flex-col gap-1'>
+              <label
+                id={`${str}-label`}
+                for={str}
+                class='block mb-2 text-active'
+              >
+                {str.charAt(0).toUpperCase() + str.slice(1)}
+              </label>
+              <input
+                type='text'
+                id={str}
+                name='text'
+                value={{ ...formData() }[str]}
+                onInput={handleInput}
+                required={true}
+                class={cx(
+                  'bg-primary/10 border border-border',
+                  'px-4 py-3 rounded-lg focus:outline-none',
+                  'focus:ring-2 focus:ring-primary w-full'
+                )}
+              />
+            </div>
           )}
-        />
+        </For>
+        <div id='message-input' class='flex flex-col gap-1'>
+          <label
+            id='message-label'
+            for='message'
+            class='block mb-2 text-active'
+          >
+            Name
+          </label>
+          <input
+            type='text'
+            id='message'
+            name='text'
+            value={formData().message}
+            onInput={handleInput}
+            required={true}
+            class={cx(
+              'bg-primary/10 border border-border',
+              'px-4 py-3 rounded-lg focus:outline-none',
+              'focus:ring-2 focus:ring-primary w-full h-24'
+            )}
+          />
+        </div>
       </form>
     </GenContainer>
   );
